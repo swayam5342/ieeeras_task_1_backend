c4d4df34a9329df72cbb067e9262c2fdd226f7f8
swayam5342 <swayamsb50@gmail.com>
2025-05-14 15:53:59 +0530

    made basic items route work

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,8 +1,15 @@
-from fastapi import APIRouter, Depends,HTTPException
+from fastapi import APIRouter,Depends,HTTPException
 from app.schemas import book
 
 root = APIRouter()
-
+db={1:{"name":"book1","author":"author1"}}
 @root.get("/")
 def read_root():
     return {"message": "Welcome to the Book API!"}
+
+@root.get("/items")
+def read_items():
+    if db:
+        return db
+    else:
+        raise HTTPException(status_code=404, detail="No items found")
